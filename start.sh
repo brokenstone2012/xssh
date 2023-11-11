b9a66a750165035b31ef4eda4e95b263fa3c890b
brokenstone2012 <brokenstone2012@gmail.com>
2023-11-11 17:28:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,4 @@ service nginx start
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
-nohup ngrok tcp 22 &&
-echo "start sunny service" &&
-chmod 777 /app/sunny &&
-setsid /app/sunny clientid 10194247400 &&
-echo "start sunny service success"
+nohup ngrok tcp 22 
