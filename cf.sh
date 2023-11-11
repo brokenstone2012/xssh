e54b13615c967a4c3089bb967ecde2d1d740544f
brokenstone2012 <brokenstone2012@gmail.com>
2023-11-11 17:27:58 +0800

    Create cf.sh

---
+++ cf.sh
@@ -0,0 +1,3 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb &&
+dpkg -i cloudflared.deb &&
+cloudflared service install eyJhIjoiOTU4MGJkOWM2MGZiM2E2MDQyMDRlYTRlNzA5NTk4NjQiLCJ0IjoiYWI4YTliNzUtZWQ5YS00ZjdlLTkzZDUtNWM4MzQ0MThiOTk1IiwicyI6Ik5XTm1PR1ZtTXpjdFlUa3dOQzAwTm1FNExXSm1PRE10WXpsalpETTRORGc0WWpVdyJ9
