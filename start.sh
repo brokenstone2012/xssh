284f5cbbaedda7c995fd542fed629d46529c1f68
brokenstone2012 <brokenstone2012@gmail.com>
2023-11-11 16:16:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
 ngrok tcp 22 --log=stdout > ngrok.log 
-# chmod 777 /app/sunny && /app/sunny clientid 10194247400
+chmod 777 /app/sunny && /app/sunny clientid 10194247400
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && dpkg -i cloudflared.deb && cloudflared service install eyJhIjoiOTU4MGJkOWM2MGZiM2E2MDQyMDRlYTRlNzA5NTk4NjQiLCJ0IjoiYWI4YTliNzUtZWQ5YS00ZjdlLTkzZDUtNWM4MzQ0MThiOTk1IiwicyI6Ik5XTm1PR1ZtTXpjdFlUa3dOQzAwTm1FNExXSm1PRE10WXpsalpETTRORGc0WWpVdyJ9
